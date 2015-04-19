a0532341a2bf4c83ea780828d13f8d3dd874e7a5
Oiva Eskola <oiva.eskola@gmail.com>
2015-04-20 00:28:26 +0300

    use markdown for texts

--- app/scripts/components/topic.js
+++ app/scripts/components/topic.js
@@ -1,39 +1,18 @@
 'use strict';
 
 var React = require('react'),
-    _ = require('lodash');
+    markdown = require('markdown').markdown;
 
 var TopicView = React.createClass({
 
   render: function() {
-    var previousTag, paragraphs = this.props.topic.text;
-    _.each(paragraphs, function(p, i) {
-      if (p.substring(0, 2) === '- ') {
-        if (previousTag === 'li') {
-          paragraphs[i - 1] += '\n'+p;
-          paragraphs[i] = null;
-        }
-        previousTag = 'li';
-      }
-    });
-
-    var text = _.map(paragraphs, function(p, i) {
-      if (p === null) {
-        return;
-      }
-      if (p.substring(0, 2) === '- ') {
-        return <ul key={i}>{_.map(p.split('\n'), function(line, j) {
-          return <li key={j}>{line.substring(2)}</li>;
-        })}</ul>
-      }
-      return <p key={i}>{p}</p>;
-    });
-
+    var html = markdown.toHTML(this.props.topic.text);
+    
     return (
       <div className="row topic-view">
         <div className="col-md-12">
           <h2>{this.props.topic.title}</h2>
-          {text}
+          <div dangerouslySetInnerHTML={{__html: html}} />
         </div>
       </div>
     );
