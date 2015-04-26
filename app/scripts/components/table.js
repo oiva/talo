908e490217e9a9c6a3ac82df356abd03309b5215
Oiva Eskola <oiva.eskola@gmail.com>
2015-04-26 15:28:50 +0300

    more details

--- app/scripts/components/table.js
+++ app/scripts/components/table.js
@@ -5,20 +5,31 @@ var React = require('react'),
 
 var TableView = React.createClass({
   render: function() {
+    var rows = _.map(this.props.data, function(value, key) {
+      var title, text;
+      if (_.isObject(value) && typeof value.text !== 'undefined' && typeof value.year !== 'undefined') {
+        title = value.year;
+        text = value.text;
+      } else {
+        title = key;
+        text  = value;
+      }
+
+      return (
+        <tr key={key}>
+          <th>{title}</th>
+          <td>{text}</td>
+        </tr>
+      );
+    });
+
     return (
       <div className="row">
         <div className="col-md-12">
           <h2>{this.props.title}</h2>
           <table>
             <tbody>
-              {_.map(this.props.data, function(value, key) {
-                return (
-                  <tr key={key}>
-                    <th>{key}</th>
-                    <td>{value}</td>
-                  </tr>
-                );
-              })}
+              {rows}
             </tbody>
           </table>
         </div>
