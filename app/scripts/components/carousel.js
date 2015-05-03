5ea504c95aabcdaa96b1e2469312a4220b82153d
Oiva Eskola <oiva.eskola@gmail.com>
2015-05-03 12:30:51 +0300

    replace carousel to save memory

--- app/scripts/components/carousel.js
+++ app/scripts/components/carousel.js
@@ -2,70 +2,94 @@
 
 var React = require('react'),
     _ = require('lodash'),
-    $ = jQuery,
-    Slider = require('react-slick');
+    Carousel = require('merry-go-round/Carousel');
 
-var SimpleSlider = React.createClass({
-  render: function () {
-    var settings = {
-      adaptiveHeight: true,
-      autoplay: false,
-      dots: true,
-      draggable: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
+var MAX_WIDTH = 700/*px*/;
+var MARGIN = 15/*px*/;
+var ANIMATION_DURATION = 250/*ms*/;
+
+var PageView = React.createClass({
+  render: function() {
     return (
-      <Slider {...settings}>
-        {_.map(this.props.photos, function(photo, key) {
-          var url = '/images/carousel/' + photo;
-          return (
-            <div key={key}>
-              <a className="carousel-link" href={url}>
-                <img className="carousel-image" src={url} alt="" />
-              </a>
-            </div>
-          )
-        })}
-      </Slider>
+      <a href={this.props.page.url}>
+        <img src={this.props.page.url} />
+      </a>
     );
   }
 });
 
 var CarouselView = React.createClass({
-  scrollCallback: null,
-  previousOffset: 0,
-
-  componentDidMount: function() {
-    var top = $(this.getDOMNode()).offset().top;
-
-    this.scrollCallback = _.debounce(function() {
-      console.log('callback 2');
-      var offset = $(window).scrollTop();
-      if (offset < top + 500 && this.previousOffset > top + 525) {
-        // reflow when scrolling back up
-        console.log('reflow');
-        $(this.getDOMNode()).css('transform', 'translateZ(0)');
-        window.removeEventListener('scroll', this.scrollCallback);
-      }
-      this.previousOffset = offset;
-    }.bind(this), 250);
-
-    window.addEventListener('scroll', this.scrollCallback);
+  getInitialState () {
+    return {
+      screenWidth: window.innerWidth,
+      pageIndex: 0,
+      previousPageIndex: 0
+    };
   },
 
-  componentWillUnmount: function() {
-    window.removeEventListener('scroll', this.scrollCallback);
+  updateScreenSize () {
+    this.setState({
+      screenWidth: window.innerWidth
+    });
+  },
+
+  componentDidMount () {
+    window.addEventListener('resize', this.updateScreenSize, false);
+  },
+
+  handleSwipe:function (event) {
+    var pageIndex = this.state.pageIndex + event.sign;
+    this.setState({ pageIndex: pageIndex });
+
+    clearTimeout(this.animationTimer);
+    this.animationTimer = setTimeout(function()  {
+      this.setState({ previousPageIndex: pageIndex });
+    }.bind(this), ANIMATION_DURATION);
+  },
+
+  handleNext: function(event) {
+    var pageIndex = this.state.pageIndex + 1;
+    this.setState({ pageIndex: pageIndex });
+    event.preventDefault();
+  },
+
+  handlePrev: function(event) {
+    var pageIndex = this.state.pageIndex -1;
+    this.setState({ pageIndex: pageIndex });
+    event.preventDefault();
   },
 
   render: function() {
+    var pages = _.map(this.props.photos, function(photo) {
+      return {url: '/images/carousel/' + photo};
+    });
+
+    var width = Math.min(this.state.screenWidth, MAX_WIDTH);
+    var pageWidth = width;
+    width -= 2*MARGIN;
+    var height = Math.ceil(pageWidth / 4 * 3);
+
     return (
       <div className="carousel row">
         <div className="col-md-12">
           <h2>Katso kuvat!</h2>
-          <SimpleSlider photos={this.props.photos} />
+          <div className="carousel-wrapper">
+            <Carousel
+              pages={pages}
+              pageIndex={this.state.pageIndex}
+              previousPageIndex={this.state.previousPageIndex}
+              width={width}
+              height={height}
+              embedWidth={0}
+              pageView={PageView}
+              pageWidth={pageWidth}
+              pageHeight={height}
+              onSwiped={this.handleSwipe}
+              loop={true}
+            />
+            <a href="#" className="carousel-button prev" onClick={this.handlePrev} />
+            <a href="#" className="carousel-button next" onClick={this.handleNext} />
+          </div>
         </div>
       </div>
     );
