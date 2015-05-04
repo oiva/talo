9d97eeb9f67251c6e6ec9370b7254e15ec8b9bbb
Oiva Eskola <oiva.eskola@gmail.com>
2015-05-04 22:28:00 +0300

    add link to some map

--- app/scripts/components/files.js
+++ app/scripts/components/files.js
@@ -6,17 +6,22 @@ var React = require('react'),
 var FilesView = React.createClass({
 
   render: function() {
-    
+    var files = _.map(this.props.files, function(value, key) {
+      var target = '';
+      if (value.substring(0, 1) !== '/') {
+        target = '_blank';
+      }
+      return (
+        <li key={key}><a href={value} target={target}>{key}</a></li>
+      )
+    });
+
     return (
       <div className="row files-view">
         <div className="col-md-12">
           <h2>Tiedostot</h2>
           <ul className="files">
-          {_.map(this.props.files, function(value, key) {
-            return (
-              <li key={key}><a href={value}>{key}</a></li>
-            )
-          })}
+            {files}
           </ul>
         </div>
       </div>
